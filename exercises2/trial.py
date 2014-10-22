987535120704f8e0738dc4c0b3140ccd6042a938
Marius Berge Eide <mariusbe@gmail.com>
2014-10-22 15:57:16 +0200

    Runge Kutta implemented. Runs:
    - no pressure
    - peak in momentum rhov
    - constant density
    - plots derivative operator

--- exercises2/trial.py
+++ exercises2/trial.py
@@ -21,6 +21,40 @@ def eos(rho, gamma):
 def ecsolver(f, dx, dt, xi, yi):
     """ Advances one step in time """
     y1 = yi + dt * f(xi, yi, dx, dt)
+    
+    rho  = yi[0]
+    p    = yi[1]
+    p_g  = yi[2]
+    Q    = yi[3]
+    #Q    = deriv( deriv( deriv( deriv(p, dx, direction=-1), dx), dx, direction=-1), dx)
+
+    #y1[4] =     -       p / interp(p, direction=1) \
+    #                   *deriv( interp(p, direction=-1), dx)  \
+    y1[4] =       -       deriv((p_g + Q), dx) \
+    #            +       interp(rho, direction=1) * g_z
+
+    return y1
+
+def rk4solver(f, dx, dt, xi, yi):
+    """ Advances one step in time using a predictor-corrector method """
+
+    k1 = f(xi, yi, dx, dt)
+    k2 = f(xi, yi + dt/2 * k1, dx, dt/2)
+    k3 = f(xi, yi + dt/2 * k2, dx, dt/2)
+    k4 = f(xi, yi + dt   * k3, dx, dt)
+
+    y1 = yi + dt/6 * (k1 + 2.*k2 + 2.*k3 + k4)
+    
+    rho  = yi[0]
+    p    = yi[1]
+    p_g  = yi[2]
+    Q    = yi[3]
+    #Q    = deriv( deriv( deriv( deriv(p, dx, direction=-1), dx), dx, direction=-1), dx)
+
+    y1[4] =     -       p / interp(p, direction=1) \
+                       *deriv( interp(p, direction=-1), dx)  \
+           -       deriv((p_g + Q), dx) \
+                +       interp(rho, direction=1) * g_z
 
     return y1
 
@@ -33,25 +67,25 @@ def cont_eqs(xi, yi, dx, dt):
 
     gamma = 5./3
     
-    p_g  = zeros(N) # eos(rho, gamma)
+    p_g  = 0 #eos(rho, gamma)
 
     dpdt =      -       p / interp(p, direction=1) \
-                       *deriv( interp(p, direction=-1), dx) \
-                -       deriv((p_g + Q), dx) \
+                    *deriv( interp(p, direction=-1), dx) \
+        -       deriv((p_g + Q), dx) \
                 +       interp(rho, direction=1) * g_z
 
     drhodt =    -       deriv( p, dx)
 
-    dp_gdt =            zeros(N) # gamma * power(rho, gamma-1) * drhodt
+    dp_gdt =    zeros(N) #          gamma * power(rho, gamma-1) * drhodt
 
-    return array([drhodt, dpdt, dp_gdt, zeros(N)])
+    return array([drhodt, dpdt, dp_gdt, zeros(N),    zeros(N)])
 
 ### ANIMATION ###
 
 
 def get_state(yinout):
 
-    yinout[:] = ecsolver(f=cont_eqs, dx=dx, dt=dt, xi=0, \
+    yinout[:] = rk4solver(f=cont_eqs, dx=dx, dt=dt, xi=0, \
             yi=yinout)
 
     return (x, yinout)
@@ -59,19 +93,19 @@ def get_state(yinout):
 # y is THE array, holding values that will be integrated in time
 
 N = 1024
-dt= 0.00001
+dt= 0.001
 
 x = linspace(0,1,N)
 dx= x[1] - x[0]
 
-rhov  = ones(N)
-rhov[where((x < 0.7) & (x > 0.4))] = 2.0
-rho = ones(N) #ones(N) + peak(N/2, N/6, N, 3)
+rho  = ones(N)
+#rhov[where((x < 0.7) & (x > 0.4))] = 2.0
+rhov = ones(N) + peak(N/2, N/6, N, 3)
 p_g  = zeros(N) #eos(rho, 5./3)
 Q    = zeros(N)
 g_z  = 0.0
 
-y    = zeros((4,N))
+y    = zeros((5,N)) ####### 
 y[0] = rho
 y[1] = rhov
 y[2] = p_g
@@ -83,36 +117,68 @@ minval = min(x)
 maxval = max(x)
 
 # set up axes and plot empty data set
-ax  = plt.axes(xlim=(minval, maxval), ylim=(-0.5,3))
+ax  = plt.axes(xlim=(minval, maxval), ylim=(-2.5,13))
 line_rho,   = ax.plot([], [], lw=2)
 line_rv,    = ax.plot([], [], lw=2)
 line_p_g,   = ax.plot([], [], lw=2)
-lines = [line_rho, line_rv, line_p_g]
+line_diffop,   = ax.plot([], [], lw=2)
+lines = [line_rho, line_rv, line_p_g, line_diffop]
 
 def init():
     for line in lines:
         line.set_data([], [])
-    ax.legend([line_rho, line_rv, line_p_g], \
-            [r'$\rho$', r'$\rho v$', r'$p_{\rm gas}$'])
+    ax.legend(lines, \
+            [r'$\rho$', r'$\rho v$', r'$p_{\rm gas}$', 'diffoperator'])
     return lines
 
 def animate(it, y, lines):
     x, y = get_state(y)
     rho, rhov, p_g, Q   = y[0], y[1], y[2], y[3]
+    diffterm = y[4] ######
     lines[0].set_data(x, rho)
     lines[1].set_data(x, rhov)
     lines[2].set_data(x, p_g)
+    lines[3].set_data(x, diffterm)
     return lines
 
 
 anim = animation.FuncAnimation(fig, animate, init_func=init, \
-        fargs=(y, lines), frames=64, interval=25, blit=True)
+        fargs=(y, lines), frames=1024, interval=05, blit=True)
 
 # To save, uncomment following line
 # Requires packages ffmpeg, mencoder and libx264 with dependencies
 print 'Writing animation to file...'
-#anim.save('ec_test.mp4', fps=25, extra_args=['-vcodec', 'libx264', '-profile:v', 'baseline'])
+#anim.save('no_press_rhov.mp4', fps=125, extra_args=['-vcodec', 'libx264'])
 print 'Done'
 
 # the show goes on forever:
 plt.show()
+
+
+
+
+
+
+
+## TEST SUITE:
+#
+## Test time integrator
+
+#def linear(xi, yi, dx, dt):
+#    return yi
+#
+#N = 1024
+#tmparr = zeros(N)
+#ytmp =  zeros((N,2))
+#ytmp[0,:] = 1.0
+#for i in xrange(N-1):
+#    ytmp[i+1,0] = ecsolver(linear, dx,  10*dt, 0.0, ytmp[i,0])
+#    ytmp[i+1,1] = rk4solver(linear, dx, 10*dt, 0.0, ytmp[i,1])
+#    tmparr[i+1] = tmparr[i] + 10*dt
+#
+#plt.plot(tmparr, ytmp[:,0])
+#plt.plot(tmparr, ytmp[:,1])
+#plt.plot(tmparr, exp(tmparr))
+#plt.show()
+#
+#
